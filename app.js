b0da5c2cb1218995d9790518ff2652beff15dc27
Verano <kamoteka143din@gmail.com>
2020-10-31 10:07:39 +0800

    add favourite music section

---
+++ app.js
@@ -0,0 +1,4 @@
+//const myFirstTry = document.getElementsByClassName("Dave_photo");
+//console.log(myFirstTry);
+//const mySecondTry = document.querySelectorAll("li.name");
+//console.log(mySecondTry);
