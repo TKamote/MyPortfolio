bcd2de66ee855bad341d1ebbbfd0134ed8cf8b7d
Verano <kamoteka143din@gmail.com>
2023-01-01 17:17:38 +0800

    2023 commit to add my sample portfolio website

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("dave")
+
+const submit = document.getElementById('submit');
+
+const creatElement = document.createElement('p');
+const createText = document.createTextNode('Buang si nono');
+creatElement.appendChild(createText);
+const getElement = document.querySelector('.social-media');
+
+
+
+submit.addEventListener('click', () => {
+    getElement.appendChild(creatElement);
+})
+
